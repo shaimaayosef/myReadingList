1e289a2abe191bfccf016d3843639d31f0ef1a12
shaimaayosef <shimaa.sayed8888@gmail.com>
2024-08-25 02:25:48 +0300

    show book

--- src/pages/ShowBook.jsx
+++ src/pages/ShowBook.jsx
@@ -1,18 +1,39 @@
-
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 import './ShowBook.css';
 
 const ShowBook = () => {
+  const [book,setBook] = useState(null);
+  const { id } = useParams();
+
+  
+
+  useEffect(() => {
+    axios.get(`http://localhost:5000/books/${id}`)
+    .then((res)=>{
+      setBook(res.data);
+    })
+    .catch((error)=>{
+      console.log(error);
+    })
+  }, [id]);
+  if (!book) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="show-book-container">
       <div className='right-div'>
-        <h2>title</h2>
-        <p>author</p>
-        <p>year</p>
-        <img src="" alt="" />
-        <p>description</p>
+        <h2>{book.title}</h2>
+        <p>{book.author}</p>
+        <p>{book.year}</p>
+        <img src={book.image} alt={book.title} />
+        <p>{book.description}</p>
       </div>
+      {/* <p>Status: {book.isRead ? 'Read' : 'Not Read'}</p> */}
       <div  className="left-div">
-        <p>Notes: notes</p>
+        <p>Notes: {book.notes}</p>
       </div>
     </div>
   );
